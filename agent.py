f012de3570d5dcd44cfe270ba9e09196a4c2c109
mohiuddin1221 <topumohiuddin3@gmail.com>
2025-07-10 15:20:45 +0600

    Add nodes and conditional edges to LangGraph workflow

--- agent.py
+++ agent.py
@@ -9,6 +9,7 @@ from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, System
 from langchain.schema import Document
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import AzureChatOpenAI
+from langgraph.graph import StateGraph, START, END
 
 llm = AzureChatOpenAI(
     openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
@@ -221,3 +222,51 @@ def off_topic_response(state:Agentstate):
     print("Entering off topic response")
     state["messages"].append(AIMessage(content= "I'm sorry! I cannot answer this question!"))
     return state
+
+
+
+
+#####Workflow#####
+workflow = StateGraph(Agentstate)
+workflow.add_node("question_rewriter", question_rewriter)
+workflow.add_node("question_classifier", question_classifier)
+
+workflow.add_node("retrieve", retrieve)
+workflow.add_node("retrieval_grader", retrieval_grader)
+workflow.add_node("refine_question", refine_question)
+workflow.add_node("generate_answer", generate_answer)
+workflow.add_node("cannot_answer", cannot_answer)
+workflow.add_node("off_topic_response", off_topic_response)
+
+###worflow edge
+workflow.add_edge(START, "question_rewriter")
+workflow.add_edge("question_rewriter", "question_classifier")
+workflow.add_conditional_edges(
+    "question_classifier",
+    on_topic_router,
+    {
+        "retrieve", retrieve,
+        "off_topic_response", off_topic_response
+    }
+
+
+)
+workflow.add_edge("retrieve", "retrieval_grader")
+workflow.add_conditional_edges(
+    "retrieval_grader",
+    proced_router,
+    {
+        "refine_question", refine_question,
+        "generate_answer", generate_answer,
+        "cannot_answer", cannot_answer
+
+    }
+)
+
+workflow.add_edge("refine_question", "retrieve")
+workflow.add_edge("generate_answer", END)
+workflow.add_edge("cannot_answer", END)
+workflow.add_edge("off_topic_response", END)
+
+
+graph = workflow.compile()
